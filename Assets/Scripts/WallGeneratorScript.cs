0387f5d20688c83ebeeab12d8ebecbea52da9a89
そうた <sota48690426@gmail.com>
2024-09-20 10:27:53 +0900

    ボタン消去完成
    一回ボタンを押したら消去できるようになりました。

--- Assets/Scripts/WallGeneratorScript.cs
+++ Assets/Scripts/WallGeneratorScript.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WallGeneratorScript : MonoBehaviour
 {
     public GameObject wall;
+
+    public Button StartButton;
+
     float timer = 0.0f;
     int interval = 12;
     int button = 0;
@@ -31,5 +35,6 @@ public class WallGeneratorScript : MonoBehaviour
         button = 1;
         Instantiate(wall, transform.position, transform.rotation);
         timer = 0;
+        StartButton.gameObject.SetActive(false);
     }
 }
